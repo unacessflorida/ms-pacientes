d99a15e6faba0b19906ad8c709eb27bc348b7504
felipefvega <datfiliv@gmail.com>
2026-07-13 12:14:57 -0400

    feat: agrega eureka client

--- src/main/java/cl/unaccess/pacientes/controller/PacienteController.java
+++ src/main/java/cl/unaccess/pacientes/controller/PacienteController.java
@@ -13,7 +13,6 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import java.util.List;
-import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/v1/pacientes")
